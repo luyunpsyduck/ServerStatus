2791c0b24f34a6137f745f5685e4aae203523435
luyunpsyduck <48854040+luyunpsyduck@users.noreply.github.com>
2019-03-25 01:57:05 +0800

    Update client-psutil.py

--- clients/client-psutil.py
+++ clients/client-psutil.py
@@ -3,14 +3,13 @@
 # 依赖于psutil跨平台库：
 # 支持Python版本：2.6 to 3.5 (users of Python 2.4 and 2.5 may use 2.1.3 version)
 # 支持操作系统： Linux, Windows, OSX, Sun Solaris, FreeBSD, OpenBSD and NetBSD, both 32-bit and 64-bit architectures
-
-SERVER = "127.0.0.1" #改成呢你的服务器地址
-PORT = 3561
-USER = "USER" #改成唯一的客户端用户名，服务器根据这个字段判断是哪台服务器
-PASSWORD = "USER_PASSWORD" #修改你的密码，和其他客户端可以是相同的
-
-INTERVAL = 1 # 请勿修改
 import socket
+SERVER = "172.105.202.12" #改成呢你的服务器地址
+PORT = 3561
+USER =  socket.gethostname()
+PASSWORD = "st8884556" #修改你的密码，和其他客户端可以是相同的
+print(USER)
+INTERVAL = 1 # 请勿修改
 import time
 import string
 import math
